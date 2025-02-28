56d8245b038a029774ee4d55ff798f57ec9b7e36
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-02-28 20:00:53 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,32 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int goodString(String s) {
+        int minOperations = -1;
+		for (int i = 0; i < 26; i++) {
+			char c = (char) (i + 'a');
+       
+            int firstIndex = s.indexOf(c);
+            int lastIndex = s.lastIndexOf(c);
+            
+            
+            if (firstIndex == -1 || lastIndex == -1) {
+                break;
+            }
+            
+            
+            int operations = firstIndex + (s.length() - lastIndex - 1);
+            
+            
+            if (minOperations == -1 || operations < minOperations) {
+                minOperations = operations;
+            }
+        }
+        
+
+		
+		return minOperations;
+	}
 
 }
+
